8dca50787a2a14564f377aedd1c3ef8fcd0d7808
StancaRadu <radu.hakuo@gmail.com>
2025-02-06 19:28:07 +0200

    close button

--- open-tabs.js
+++ open-tabs.js
@@ -1,10 +1,11 @@
 tabs = document.querySelectorAll('.side-tab')
 clicked = false
-tabs.forEach(tab => {
-    tab.onclick = function (e){
-        if (clicked) return
+
+function changeTab(tab, title = false) {
+    if (clicked) return
         clicked = true
-        new_title = this.getElementsByTagName('h2')[0].innerHTML
+        if (!title) new_title = tab.getElementsByTagName('h2')[0].innerHTML
+        else new_title = title
         changer = document.getElementById('title-changer').style
         changer.left = '-100%'
         setTimeout(()=>{
@@ -25,6 +26,11 @@ tabs.forEach(tab => {
                 }, 500)
             }, 500)
         }, 500)
+}
+
+tabs.forEach(tab => {
+    tab.onclick = function (){
+        changeTab(tab)
     }
 });
 
@@ -33,4 +39,8 @@ document.getElementById('country1').onclick = function(e){
 }
 document.getElementById('country2').onclick = function(e){
     document.getElementById('slider').style.paddingLeft = '62.5%'
+}
+
+document.getElementById('close-button').onclick = function(e){
+    changeTab(false, title = 'Welcome!')
 }
