a66e9c22e22185b0af198d3b6643bcd30b20c48d
StancaRadu <radu.hakuo@gmail.com>
2025-02-06 06:54:05 +0200

    part of side menu

--- open-tabs.js
+++ open-tabs.js
@@ -1,6 +1,9 @@
 tabs = document.querySelectorAll('.side-tab')
+clicked = false
 tabs.forEach(tab => {
     tab.onclick = function (e){
+        if (clicked) return
+        clicked = true
         new_title = this.getElementsByTagName('h2')[0].innerHTML
         changer = document.getElementById('title-changer').style
         changer.left = 0
@@ -12,8 +15,16 @@ tabs.forEach(tab => {
                 changer.left = '-200%'
                 setTimeout(()=>{
                     changer.transitionDuration = '1s'
+                    clicked = false
                 }, 10)
             }, 1000)
         }, 1000)
     }
 });
+
+document.getElementById('country1').onclick = function(e){
+    document.getElementById('slider').style.paddingLeft = '12.5%'
+}
+document.getElementById('country2').onclick = function(e){
+    document.getElementById('slider').style.paddingLeft = '62.5%'
+}
