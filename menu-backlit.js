51d6deb787da164b8a68e4f489e97bfe688c9ec4
StancaRadu <radu.hakuo@gmail.com>
2025-02-06 17:32:59 +0200

    changed hover element for titles

--- menu-backlit.js
+++ menu-backlit.js
@@ -1,12 +1,8 @@
 let menu = document.getElementById('side-titles')
-tabs = document.querySelectorAll('.side-tab')
-clicked = false
-tabs.forEach(tab => {
-    tab.onmousemove = function (e){
-        let x = e.pageX - menu.offsetLeft;
+menu.onmousemove = function (e) {
+    let x = e.pageX - menu.offsetLeft;
     let y = e.pageY - menu.offsetTop;
 
     menu.style.setProperty('--y', `${y}px`)
     menu.style.setProperty('--x', `${x}px`)
-    }
-});
+}
